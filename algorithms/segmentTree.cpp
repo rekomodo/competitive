32a4c31ba7005ade730d9c2923992bc000d1cb64
rekomodo <43099154+rekomodo@users.noreply.github.com>
2023-01-05 22:42:05 -0400

    updated segment tree implementation

--- algorithms/segmentTree.cpp
+++ algorithms/segmentTree.cpp
@@ -1,45 +1,34 @@
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 #define pb push_back
-#define all(s) s.begin(), s.end()
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> pii;
 
-// segment tree implementation my way, max segment tree will be used for example
-
-const int INF = 1e9;
-
-struct node
+template <class Node>
+struct SegmentTree
 {
-    int value;
-    node(int _value = -INF) : value(_value) {}
-};
+    // generic segmentTree functions
 
-struct segmentTree
-{
-    int size, power2 = 1;
-    node nullNode = node();
-    vector<node> tree;
+    vector<Node> tree;
+    int power2, treeSize;
 
-    segmentTree(int n, node _nullNode)
+    virtual Node merge(Node &a, Node &b) = 0;
+
+    SegmentTree(int n)
     {
-        nullNode = _nullNode;
+        power2 = 1;
         while (power2 < n)
             power2 *= 2;
-        size = 2 * power2 - 1;
-        tree = vector<node>(size + 1, _nullNode);
+
+        treeSize = 2 * power2 - 1;
+        tree.resize(treeSize + 1, Node());
     }
 
-    virtual node merge(node &a, node &b) = 0;
-
-    void update(int i, node v)
+    void update(int i, Node k)
     {
-        int in = i + size / 2;
-        tree[in] = v;
+        int in = i + treeSize / 2;
+        tree[in] = k;
 
         in /= 2;
+
         while (in > 0)
         {
             tree[in] = merge(tree[in * 2], tree[in * 2 + 1]);
@@ -47,85 +36,35 @@ struct segmentTree
         }
     }
 
-    node query(int l, int r, int queryL, int queryR, int i = 1)
+    Node query(int i, int l, int r, int ql, int qr)
     {
-        if (l > queryR || r < queryL)
-            return nullNode;
-        if (queryL <= l && r <= queryR)
-        {
+        if (r < ql || l > qr)
+            return Node();
+        if (ql <= l && r <= qr)
             return tree[i];
-        }
 
         int mid = (l + r) / 2;
-        node query1 = query(l, mid, queryL, queryR, i * 2);
-        node query2 = query(mid + 1, r, queryL, queryR, i * 2 + 1);
-        return merge(query1, query2);
+        Node q1 = query(i * 2, l, mid, ql, qr);
+        Node q2 = query(i * 2 + 1, mid + 1, r, ql, qr);
+        return merge(q1, q2);
+    }
+
+    Node query(int ql, int qr)
+    {
+        return query(1, 1, power2, ql, qr);
     }
 };
 
-struct maxSegmentTree : segmentTree
+struct MinNode
 {
-    maxSegmentTree(int n, node _nullNode) : segmentTree(n, _nullNode) {}
-    node merge(node &a, node &b)
-    {
-        return max(a.value, b.value);
-    }
+    int value = 1e9;
 };
 
-struct sumSegmentTree : segmentTree
+struct MinSegmentTree : SegmentTree<MinNode>
 {
-    sumSegmentTree(int n, node _nullNode) : segmentTree(n, _nullNode) {}
-    node merge(node &a, node &b)
+    MinSegmentTree(int n) : SegmentTree(n){};
+    MinNode merge(MinNode &a, MinNode &b)
     {
-        return a.value + b.value;
+        return (a.value < b.value ? a : b);
     }
 };
-
-int n, q;
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> n;
-    vi nums(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> nums[i];
-    }
-
-    maxSegmentTree maxTree = maxSegmentTree(5, node(-1e9));
-    sumSegmentTree sumTree = sumSegmentTree(5, node(0));
-    for (int i = 1; i <= n; i++)
-    {
-        maxTree.update(i, nums[i]);
-        sumTree.update(i, nums[i]);
-    }
-
-    cin >> q;
-    for (int i = 0; i < q; i++)
-    {
-        int t, l, r;
-        cin >> t >> l >> r;
-        if (t == 1)
-        {
-            int queryMax = maxTree.query(1, maxTree.power2, l, r).value;
-            int querySum = sumTree.query(1, sumTree.power2, l, r).value;
-
-            auto print = [&l, &r](string type, int result)
-            {
-                cout << type << ' ' << l << '-' << r << " : " << result << '\n';
-            };
-
-            print("max", queryMax);
-            print("sum", querySum);
-        }
-        else
-        {
-            maxTree.update(l, r);
-            sumTree.update(l, r);
-            cout << "update A[" << l << "] := " << r << '\n';
-        }
-    }
-}
